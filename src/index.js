23857a49ac55395092f449437d7bfa9806d356e7
lyj0508 <lyj0508@jainwon.com>
2022-12-08 14:19:19 +0900

    feat : 스타일드컴포넌트 실습 - 블럭만들기

--- src/index.js
+++ src/index.js
@@ -12,12 +12,13 @@ import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBook from "./chapter_10/AttendanceBook";
 import SignUp from "./chapter_11/SignUp";
 import Calculator from "./chapter_12/Calculator";
-import ProfileCard from "./chapter_13/ProfileCard";*/
-import DarkOrLight from "./chapter_14/DarkOrLight";
+import ProfileCard from "./chapter_13/ProfileCard";
+import DarkOrLight from "./chapter_14/DarkOrLight";*/
+import Blocks from "./chapter_15/Blocks";
 
 ReactDOM.render(
   <React.StrictMode>
-    <DarkOrLight />
+    <Blocks />
   </React.StrictMode>,
   document.getElementById('root')
 );
